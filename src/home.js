11c5fa04f467a0fae64b1b827ea3a7c8d41ce850
Tanishka Talwar <tanishkatalwar18@gmail.com>
2022-06-07 23:08:42 +0530

    make home page using modules and webpack

---
+++ src/home.js
@@ -0,0 +1,21 @@
+export default function createHomePage(){
+const content = document.querySelector('.content');
+
+let container = document.createElement('div');
+container.classList.add('home-container');
+
+let details = document.createElement('div');
+details.classList.add('home-details');
+
+let heading = document.createElement('h1');
+heading.appendChild(document.createTextNode('"Glorious Food"'));
+details.appendChild(heading);
+
+let description = document.createElement('p');
+description.appendChild(document.createTextNode('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti omnis, recusandae voluptatem in repudiandae. Mollitia deserunt nemo pariatur nisi quos ducimus repudiandae repellat, provident at est eveniet eligendi'));
+details.appendChild(description);
+
+container.appendChild(details);
+content.appendChild(container);
+
+}
