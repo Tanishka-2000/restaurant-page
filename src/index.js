313ee254b72e0fdd05bff893e1a283273bd95c52
Tanishka Talwar <tanishkatalwar18@gmail.com>
2022-06-08 20:28:01 +0530

    add event listener to nav links

--- src/index.js
+++ src/index.js
@@ -5,4 +5,24 @@ import createContactPage from './contact.js';
 const content = document.querySelector('.content');
 // createHomePage(content);
 //createMenuPage(content);
-createContactPage(content);
+//
+
+// functon to render Page
+function respondToClick(e){
+    if(e.target.textContent === 'home'){
+        content.innerHTML = '';
+        createHomePage(content);
+    }else if(e.target.textContent === 'menu'){
+        content.innerHTML = '';
+        createMenuPage(content);
+    }else if(e.target.textContent === 'contact'){
+        content.innerHTML = '';
+        createContactPage(content);
+    }
+}
+
+// adding event listeners to links
+const nav = document.querySelector('nav');
+nav.addEventListener('click', respondToClick);
+
+createHomePage(content);
